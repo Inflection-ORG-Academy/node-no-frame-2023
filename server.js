1954408618097675e66d527400bc82898459cad1
prakhar maurya <000.excel@gmail.com>
2023-03-23 04:03:23 +0530

    url matcher

--- server.js
+++ server.js
@@ -1,88 +1,117 @@
-const http = require('http');
-const { readFile, writeFile } = require('./file');
+const http = require('http')
+const { readFile, writeFile } = require('./file')
 
-const bodyPraser = (req) => {
-  let body = '';
+const bodyPraser = (req, res, data) => {
+  let body = ''
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      reject(new Error('body parser timeout'));
-    }, 2000);
+      reject(new Error('body parser timeout'))
+    }, 2000)
     req.on('data', function (data) {
-      body += data;
-    });
+      body += data
+    })
     req.on('end', function (data) {
-      resolve(JSON.parse(body));
-    });
-  });
-};
-
-const urlMatcher = (req, matchUrl, method) => {
-  if (req.method !== method) {
-    return false;
-  }
-  const reqUrl = req.url.split('/');
-  const url = matchUrl.split('/');
-  console.log(reqUrl, url);
-  if (reqUrl.length !== url.length) {
-    return false;
-  }
-  let isMatched = true;
-  const params = {};
-  for (let i = 0; i < url.length; i++) {
-    const x = url[i];
-    const y = reqUrl[i];
-    if (!x.includes(':')) {
-      if (x !== y) {
-        isMatched = false;
-        break;
+      try {
+        req.body = JSON.parse(body)
+      } catch (e) {
+      } finally {
+        resolve({ next: true })
       }
-    } else {
-      const z = x.split(':')[1];
-      params[z] = y;
+    })
+  })
+}
+
+const urlMatcher = (matchUrl, method) => {
+  return async (req, res, data) => {
+    if (req.method !== method) {
+      return { next: true, data }
     }
+    const reqUrl = req.url.split('/')
+    const url = matchUrl.split('/')
+    if (reqUrl.length !== url.length) {
+      return { next: true, data }
+    }
+    let isMatched = true
+    const params = {}
+    for (let i = 0; i < url.length; i++) {
+      const x = url[i]
+      const y = reqUrl[i]
+      if (!x.includes(':')) {
+        if (x !== y) {
+          isMatched = false
+          break
+        }
+      } else {
+        const z = x.split(':')[1]
+        params[z] = y
+      }
+    }
+    if (!isMatched) {
+      return { next: true, data }
+    }
+    req.params = params
+    data = await run(controllerMiddleware[`${matchUrl},${method}`], req, res)
+    return { next: true, data }
   }
-  return { isMatched, params };
-};
+}
+
+const globalMiddleware = [
+  bodyPraser,
+  urlMatcher('/students', 'GET'),
+  urlMatcher('/students', 'POST'),
+  // resChecker,
+  // errorChecker,
+]
 
 const c1 = (req, res, data) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log('c1');
-      res.end('apple');
-      resolve({ next: true, name: 'apple' });
-    }, 4000);
-  });
-};
+      console.log('c1')
+      res.end('apple')
+      resolve({ next: true, name: 'apple' })
+    }, 4000)
+  })
+}
 
 const c2 = (req, res, data) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log(data);
-      console.log('c2');
-      resolve();
-    }, 1000);
-  });
-};
+      console.log(data)
+      console.log('c2')
+      resolve()
+    }, 1000)
+  })
+}
 
-const globalMiddleware = [c1, c2];
+const controllerMiddleware = {
+  '/students,GET': [c1, c2],
+  // '/students,POST': [authCheck, work2],
+}
 
 const run = async (fnArray, req, res) => {
-  let data = undefined;
-  for (let i = 0; i < fnArray.length; i++) {
-    data = await fnArray[i](req, res, data);
-    if (data?.next) {
-      continue;
-    }
-    break;
+  if (!Array.isArray(fnArray)) {
+    return
   }
-};
+  let data = {}
+  for (let i = 0; i < fnArray.length; i++) {
+    data = await fnArray[i](req, res, data)
+    if (data?.next) {
+      continue
+    }
+    break
+  }
+  return data
+}
 
 const server = http.createServer(async (req, res) => {
   try {
-    await run(globalMiddleware, req, res);
+    await run(globalMiddleware, req, res)
   } catch (e) {
-    console.log(e);
+    res.writeHead(500, {
+      'Content-Type': 'application/json',
+    })
+    res.end(JSON.stringify({ error: e.message }))
   }
-});
+})
 
-server.listen('8080');
+server.listen('8080')
