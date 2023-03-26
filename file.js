45fbe6a479002a9264a4d583653575b21e6a2577
Pushpendra <pushpendrakumarkashyap0@gmail.com>
2023-03-26 16:14:03 +0530

    provide id in dbjson

--- file.js
+++ file.js
@@ -23,3 +23,15 @@ exports.writeFile = async (data) => {
   const contents = JSON.stringify(data);
   await fsp.writeFile(dbFileName, contents, { encoding: 'utf8' });
 };
+exports.generateId = (students) => {
+  if (!Array.isArray(students) || !students.length) {
+    return 1;
+  }
+  let max = 0;
+  for (let i = 0; i < students.length; i++) {
+    if (max < students[i].id) {
+      max = students[i].id;
+    }
+  }
+  return max + 1;
+};
--- students/controllers.js
+++ students/controllers.js
@@ -1,15 +1,15 @@
-const { readFile, writeFile } = require('../file');
+const { readFile, writeFile, generateId } = require('../file');
 
 exports.getStudents = async (req, res, data) => {
   const fileData = await readFile();
   const students = fileData.students;
   res.end(JSON.stringify(students));
 };
-
 exports.addStudent = async (req, res, data) => {
   const fileData = await readFile();
   // TODO: generate id for students before adding to students array
-  fileData.students.push(req.body);
+  const id = generateId(fileData.students);
+  fileData.students.push({ id, ...req.body });
   await writeFile(fileData);
   res.end(JSON.stringify(req.body));
 };
