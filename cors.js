5347b62db0964e57a21c50be903e924e3b694bb3
Prakhar Maurya <000.excel@gmail.com>
2023-04-11 09:24:30 +0530

    cors header update

--- cors.js
+++ cors.js
@@ -1,8 +1,11 @@
 exports.cors = (req, res, data) => {
   res.setHeader('Content-Type', 'application/json',);
+
   res.setHeader('Access-Control-Allow-Origin', '*',);
-  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,PUT,PTACH,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'X-PINGOTHER,Content-Type,X-Token');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Authorization,X-PING,Content-Type,Origin,Accept');
+  res.setHeader('Access-Control-Expose-Headers', 'Date, X-Device-Id');
+  res.setHeader('Access-Control-Max-Age', 600);
   res.setHeader('Access-Control-Allow-Credentials', true);
   return { next: true }
 };
