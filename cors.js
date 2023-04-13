930d0c4b0a70fbc966de13164e05d2e50462cfb9
Prakhar Maurya <000.excel@gmail.com>
2023-04-13 08:43:49 +0530

    fix cors error 2

--- cors.js
+++ cors.js
@@ -1,6 +1,6 @@
 exports.cors = (req, res, data) => {
   res.setHeader('Access-Control-Allow-Origin', '*',);
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Mode,access-control-allow-origin');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
   res.setHeader('Access-Control-Allow-Credentials', true);
 
